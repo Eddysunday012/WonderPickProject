02cb169b94a1ac1e857b0aa1a13bc2ba7d40e03e
Ethan Domingo <etddomin@ucsc.edu>
2025-02-11 15:27:45 -0800

    added top part

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <p className="text-3xl font-sans">Huhhh +React</p>
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex min-w-[346px] min-h-[98px] rounded-full shadow-2xl bg-white justify-center items-center">
+        <p className="text-base font-sans text-center">
+          Will poopy be my poopentine?
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
