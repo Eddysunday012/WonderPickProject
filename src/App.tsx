7dc8776c62a71f83584e608ed720cba09ddf51e4
Ethan Domingo <etddomin@ucsc.edu>
2025-02-11 18:25:56 -0800

    some more updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 // import { useState } from 'react'
 import PokemonCard from "./components/PokemonCard";
 import "./index.css";
-// import { motion } from "motion/react";
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5];
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
+    <div className="flex flex-col max-h-screen items-center justify-start gap-9">
       <div className="flex min-w-[346px] min-h-[98px] rounded-full shadow-2xl bg-white justify-center items-center">
         <p className="text-base font-sans text-center">
           Will poopy be my poopentine?
         </p>
       </div>
-      <div className="grid grid-cols-3">
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
+      <div className="grid grid-cols-3 gap-y-[124px] items-center">
+        {numbers.map((number) => (
+          <div key={number} className="grow">
+            <PokemonCard Card={number} />
+          </div>
+        ))}
       </div>
-      <div>Test if this goes through</div>
     </div>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+
 import backOfCard from "@/assets/backOfCard.png";
 import Poopymon from "@/assets/Poopymon.png";
+import Poopymon1 from "@/assets/Poopymon1.png";
+import Poopymon2 from "@/assets/Poopymon2.png";
+import Poopymon3 from "@/assets/Poopymon3.png";
+import Poopymon4 from "@/assets/Poopymon4.png";
 
-export default function PokemonCard() {
+interface Props {
+  Card: number;
+}
+
+export default function PokemonCard({ Card }: Props) {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
+
+  const cardMap = {
+    1: Poopymon,
+    2: Poopymon1,
+    3: Poopymon2,
+    4: Poopymon3,
+    5: Poopymon4,
+  } as Record<
+    number,
+    | typeof Poopymon
+    | typeof Poopymon1
+    | typeof Poopymon2
+    | typeof Poopymon3
+    | typeof Poopymon4
+  >;
 
   function handleFlip() {
     if (!isAnimating) {
@@ -15,26 +39,26 @@ export default function PokemonCard() {
   }
   return (
     <div className="flex items-center justify-center cursor-pointer">
-      <div className="flip-card w-[94px] rounded-md" onClick={handleFlip}>
+      <div className="flip-card w-[114px] rounded-md" onClick={handleFlip}>
         <motion.div
-          className="flip-card-inner w-[94px]"
+          className="flip-card-inner w-[114px]"
           initial={false}
           animate={{ rotateY: isFlipped ? 180 : 360 }}
           transition={{ duration: 0.6, animationDirection: "normal" }}
           onAnimationComplete={() => setIsAnimating(false)}
         >
           <div
-            className="flip-card-front w-[94px] bg-cover  rounded-lg p-4"
+            className="flip-card-front w-[114px] bg-cover rounded-lg p-4"
             style={{ backgroundImage: "../assets/backOfCard.png" }}
           >
             <img src={backOfCard} alt="backOfCard" />
           </div>
 
           <div
-            className="flip-card-back w-[94px] bg-cover  rounded-lg p-4"
+            className="flip-card-back w-[114px] bg-cover  rounded-lg p-4"
             style={{ backgroundImage: "../assets/Poopymon.png" }}
           >
-            <img src={Poopymon} alt="Poopymon" />
+            <img src={cardMap[Card]} alt="Poopymon" />
           </div>
         </motion.div>
       </div>
